9c03663c72d8986ba5f94596762ba900fb1b9afc
Hannah Malm <hannahemalm616@gmail.com>
2020-11-18 00:29:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 #.each 
 def reverse_each_word(string)
- string.each do |string|
-  
+  string.each.split.map do |joined_array|
+    joined_array.reverse
+  end.join(' ')
 end  
 
 #.collect 
@@ -10,23 +11,3 @@ def reverse_each_word(string)
 end  
 
 
-String str = "hello world";
-String revStr = reverseWordByWord(str);
-
- public String reverseWordByWord(String str){
-        int strLeng = str.length()-1;
-        String reverse = "", temp = "";
-
-        for(int i = 0; i <= strLeng; i++){
-            temp += str.charAt(i);
-            if((str.charAt(i) == ' ') || (i == strLeng)){
-                for(int j = temp.length()-1; j >= 0; j--){
-                    reverse += temp.charAt(j);
-                    if((j == 0) && (i != strLeng))
-                        reverse += " ";
-                }
-                temp = "";
-            }
-        }
-        return reverse;
-    }
