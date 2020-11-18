c69ad9a593f74d21c2b5861afc624b234ad8c26d
Hannah Malm <hannahemalm616@gmail.com>
2020-11-18 00:27:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,25 @@ end
 def reverse_each_word(string)
   
 end  
+
+
+String str = "hello world";
+String revStr = reverseWordByWord(str);
+
+ public String reverseWordByWord(String str){
+        int strLeng = str.length()-1;
+        String reverse = "", temp = "";
+
+        for(int i = 0; i <= strLeng; i++){
+            temp += str.charAt(i);
+            if((str.charAt(i) == ' ') || (i == strLeng)){
+                for(int j = temp.length()-1; j >= 0; j--){
+                    reverse += temp.charAt(j);
+                    if((j == 0) && (i != strLeng))
+                        reverse += " ";
+                }
+                temp = "";
+            }
+        }
+        return reverse;
+    }
