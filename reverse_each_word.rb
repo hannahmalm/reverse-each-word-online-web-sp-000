c75d3dbc2a0c07b9055c22c98bd10b8192a98309
Hannah Malm <hannahemalm616@gmail.com>
2020-11-18 00:48:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,6 @@ def reverse_each_word(string)
   #use .collect because we want a new return value instead of using .each 
   #run the joined_array iteration as if you were running it using .each 
   #and then run it in reverse
-  
   #end the join with (' ') which takes an array and returns a string value
   string.split.collect do |joined_array|
     joined_array.reverse
