4ec62b145581cee89d58ddf0c73e6aa4ca752aa7
Hannah Malm <hannahemalm616@gmail.com>
2020-11-18 00:37:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 #.collect 
 def reverse_each_word(string)
-  #.split means that while (' '') splits a string whenever there is an empty space 
+  #.split means that while (' ') splits a string whenever there is an empty space 
   string.split.collect do |joined_array|
     joined_array.reverse
   end.join(' ')
